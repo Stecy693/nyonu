0ff4e000fae73d91d71df4f52502059274eaf8b9
Stecy693 <lahitanstecy@gmail.com>
2026-02-21 04:57:25 +0100

    Add HTTPS scheme enforcement for production
    Force HTTPS scheme in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,27 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
